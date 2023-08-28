d9330232fbf704dcb996b6b5ca4ac2bf4f1f9bbd
Thindal <thindaltv@gmail.com>
2023-08-28 17:38:45 +0200

    Changing command name, should now need "dotnet bacon", not just "bacon". Upping version for nuget.orgs sake.

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.WriteLine(GetBaconStrips());
 
 static string GetBaconStrips()
